a9bb4fd5ec1be4fb82e83004666b6ead5f9fa494
DorottyaKrisztics <dorka713@gmail.com>
2017-05-01 19:48:16 +0200

    table on list.html, read from csv, write to csv

--- main.py
+++ main.py
@@ -4,23 +4,34 @@ app = Flask(__name__)
 
 
 @app.route('/story', methods=['GET', 'POST'])
-def ask_data(story_id=None):
-    datatable_list = []
+def ask_data():
+
     if request.method == 'POST':
-        story_title = request.form['story_title']
-        user_story = request.form['user_story']
-        acc_criteria = request.form['acc_criteria']
-        business_value = request.form['business_value']
-        estim = request.form['estim']
-        # Status
-        datatable_list.append(story_title + ',' + user_story + ',' + acc_criteria + ',' + business_value + ',' + estim + '\n')
+        new_line = []
+        new_line.append('id')
+        new_line.append(request.form['story_title'])
+        new_line.append(request.form['user_story'])
+        new_line.append(request.form['acc_criteria'])
+        new_line.append(request.form['business_value'])
+        new_line.append(request.form['estim'])
+        new_line.append(request.form['status'])
+
+        datatable_list = []
+        with open("data_table.csv", "r") as data_table:
+            for line in data_table:
+                cut_line = line.replace("\n", "")
+                line_list = cut_line.split(',')
+                datatable_list.append(line_list)
+        datatable_list.append(new_line)
+
         with open("data_table.csv", "w") as data_table_add:
             for item in datatable_list:
-                data_table_add.write(item)
+                line_to_csv = ','.join(item)
+                data_table_add.write(line_to_csv + "\n")
 
-        return render_template("list.html", html_list=datatable_list)
+        return render_template("list.html", html_datatable_list=datatable_list)
     else:
-        return render_template("form.html", html_list=datatable_list)
+        return render_template("form.html")
 
 
 @app.route('/story/<story_id>')
@@ -28,21 +39,18 @@ def update(story_id):
     return render_template('form.html', story_id=story_id)
 
 
+@app.route('/', methods=['GET'])
 @app.route('/list', methods=['GET'])
 def data_table():
     datatable_list = []
     with open("data_table.csv", "r") as data_table:
         for line in data_table:
-            datatable_list.append(line)
-        
+
+            cut_line = line.replace("\n", "")
+            line_list = cut_line.split(',')
+            datatable_list.append(line_list)
+
     return render_template("list.html", html_datatable_list=datatable_list)
-
-
-#def get_table_from_file(file_name):
-    #with open(file_name, "r") as file:
-        #lines = file.readlines()
-    #table = [element.replace("\n", "").split(";") for element in lines]
-    #return table
 
 
 if __name__ == '__main__':
